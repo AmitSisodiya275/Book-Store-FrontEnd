d4a6e046d3e657ee29a43919881cab1968ea4bba
unknown <gayoyawalkar@gmail.com>
2022-01-20 15:25:04 +0530

    Added:Sign up UI fro User of Book Store App

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  emailId:any;
+  password:any;
 
-  constructor() { }
+  constructor(router:Router) { }
 
   ngOnInit(): void {
+
   }
 
 }
