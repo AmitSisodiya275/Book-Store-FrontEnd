c721aecbd844c6d52f790c11704e6514c9fd810d
Naveen Pattanashetti <pattanashettinaveen.com>
2022-01-20 16:01:25 +0530

    Added:Reset passward UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 const routes: Routes = [
   { path: 'login', component:LoginComponent},
   { path: 'sign-up', component: SignUpComponent},
+  { path: 'reset', component: ResetPasswordComponent},
   { path: '' , redirectTo: 'login', pathMatch: 'full'}
 ];
 
